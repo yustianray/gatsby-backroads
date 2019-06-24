6f00d5db624c74bc3dbb2e66aef79b56a9acf53d
Yustian <ray.yustian@gmail.com>
2019-06-24 17:19:57 +0700

    Initial Commit

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const blog = () => {
+  return (
+    <div>
+      Hello from blog page <Link to="/">back home</Link>
+    </div>
+  )
+}
+
+export default blog
---
+++ src/pages/tours.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class tours extends Component {
+    render() {
+        return (
+            <div>
+                Hello from tours page
+            </div>
+        )
+    }
+}
