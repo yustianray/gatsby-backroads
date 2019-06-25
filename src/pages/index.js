30e9ff65aac69a157de0decfc419ed4e0d5c0c68
Yustian <ray.yustian@gmail.com>
2019-06-25 15:40:24 +0700

    Adding Global CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from '../components/Layout.js'
 
 export default () => (
     <Layout>
-    Hello world! <Link to="/blog/">Blog Page</Link>
-    <div className="container">
-        <div className="row text-center">
-            <div className="col-md-4">This</div>
-            <div className="col-md-4">Is</div>
-            <div className="col-md-4">Bootstrap</div>
-        </div>
-    </div>
+    <div>Hello from backroads</div>
     </Layout>
 )
