99872be8e53da6dc86208cb3c36e36e7629b30f5
Yustian <ray.yustian@gmail.com>
2019-06-25 09:58:06 +0700

    Adding bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,5 +6,12 @@ import Layout from '../components/Layout.js'
 export default () => (
     <Layout>
     Hello world! <Link to="/blog/">Blog Page</Link>
+    <div className="container">
+        <div className="row text-center">
+            <div className="col-md-4">This</div>
+            <div className="col-md-4">Is</div>
+            <div className="col-md-4">Bootstrap</div>
+        </div>
+    </div>
     </Layout>
 )
