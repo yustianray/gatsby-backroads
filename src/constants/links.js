3062d21473eec37995b656b18f82d01494d00f16
Yustian <ray.yustian@gmail.com>
2019-07-01 22:23:37 +0700

    Create Navbar

---
+++ src/constants/links.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        path: "/",
+        text: "home"
+    },
+    {
+        path: "/tours",
+        text: "tour"
+    },
+    {
+        path: "/blog",
+        text: "blog"
+    },
+    {
+        path: "/contact",
+        text: "contact"
+    }
+]
