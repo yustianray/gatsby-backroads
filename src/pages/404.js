c283dcd78336caeb67d0dcd2dbce61181fb74180
Yustian <ray.yustian@gmail.com>
2019-07-03 17:39:07 +0700

    Adding error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,19 @@
 import React from "react"
-import Layout from '../components/Layout.js'
+import Layout from "../components/Layout.js"
+import styles from "../css/error.module.css"
+import { Link } from "gatsby"
+import Banner from "../components/Banner.js"
 
 export default function error() {
-  return <Layout>Hello From Error Page</Layout>
+  return (
+    <Layout>
+      <header className={styles.error}>
+        <Banner title="oops it's dead end">
+          <Link to="/" className="btn-white">
+            back to home page
+          </Link>
+        </Banner>
+      </header>
+    </Layout>
+  )
 }
