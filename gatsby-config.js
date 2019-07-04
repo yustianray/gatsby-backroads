dccd0ecefbe4d12334c925f18300930f4e0ae0dd
Yustian <ray.yustian@gmail.com>
2019-07-04 11:53:53 +0700

    Adding siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,11 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: "BackRoads",
+    description: "Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure",
+    author: "@yustianray",
+  },
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`
