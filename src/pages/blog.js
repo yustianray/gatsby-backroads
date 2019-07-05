b858fad81e0559005fdd89af43b23de755345851
Yustian <ray.yustian@gmail.com>
2019-07-06 05:56:33 +0700

    About Image Fix

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from '../components/Layout'
-import Images from "../examples/Images"
 
 const blog = ({data}) => {
 
   return (
     <Layout>
       Hello from blog page <Link to="/">back home</Link>
-      <Images />
     </Layout>
   )
 }
