fb13a679d97dc30171874b824b4ed1d23a77b2d7
Yustian <ray.yustian@gmail.com>
2019-07-05 20:49:18 +0700

    Adding Gatsby Image

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,8 @@ module.exports = {
         path: `${__dirname}/src/images/`
       }
     },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`
   ],
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from '../components/Layout'
+import Images from "../examples/Images"
 
 const blog = ({data}) => {
 
   return (
     <Layout>
       Hello from blog page <Link to="/">back home</Link>
+      <Images />
     </Layout>
   )
 }
