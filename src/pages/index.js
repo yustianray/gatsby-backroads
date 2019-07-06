111bf70b8988f9c650e587e8ba283bbe639cb236
Yustian <ray.yustian@gmail.com>
2019-07-06 08:26:03 +0700

    Setup Home Page Background

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,34 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from '../components/Layout.js'
-import SimpleHero from "../components/SimpleHero"
 import Banner from "../components/Banner"
 import About from "../components/Home/About"
 import Services from "../components/Home/Services"
+import StyledHero from "../components/StyledHero"
+import { graphql } from "gatsby"
 
-export default () => (
+export default ({ data }) => (
     <Layout>
-        <SimpleHero>
+        <StyledHero home="true" img={data.defaultBcg.childImageSharp.fluid}>
             <Banner title="continue exploring" info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, pariatur.">
                 <Link to="/tours" className="btn-white">
                     explore tours
                 </Link>
             </Banner>
-        </SimpleHero>
+        </StyledHero>
         <About />
         <Services />
     </Layout>
 )
+
+export const query = graphql`
+    query {
+        defaultBcg:file(relativePath:{eq:"defaultBcg.jpeg"}) {
+            childImageSharp {
+                fluid(quality:90, maxWidth:4160) {
+                    ...GatsbyImageSharpFluid_withWebp
+                }
+            }
+        }
+    }
+`
