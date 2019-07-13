e0c546fcf853916e027a49c39fdea34b93167cb9
Yustian <ray.yustian@gmail.com>
2019-07-13 08:50:04 +0700

    Image default props

--- src/components/Tours/Tour.js
+++ src/components/Tours/Tour.js
@@ -4,12 +4,26 @@ import styles from "../../css/tour.module.css"
 import { FaMap } from "react-icons/fa"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+const getImage = graphql`
+    query {
+        file(relativePath: { eq: "defaultBcg.jpeg" }) {
+            childImageSharp {
+                fluid {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+    }
+`
 
 const Tour = ({ tour }) => {
+    const data = useStaticQuery(getImage)
+    const img = data.file.childImageSharp.fluid
     const { name, price, country, days, slug, images } = tour
-    let mainImage = images[0].fluid
-    console.log(country);
     
+    let mainImage = images ? images[0].fluid : img
 
     return (
         <article className={styles.tour}>
@@ -24,9 +38,9 @@ const Tour = ({ tour }) => {
                 <div className={styles.info}>
                     <h4 className={styles.country}>
                         <FaMap className={styles.icon} />
-                        {country}
+                        {country || "default country"}
                     </h4>
-                    <div className={styles.details}>
+                     <div className={styles.details}>
                         <h6>{days} days</h6>
                         <h6>from ${price}</h6>
                     </div>
