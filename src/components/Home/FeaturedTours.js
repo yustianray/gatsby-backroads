72f633aa7db8b77cdc342b8f017cec907606b7cd
Yustian <ray.yustian@gmail.com>
2019-07-14 17:24:02 +0700

    Blog Card Component

--- src/components/Home/FeaturedTours.js
+++ src/components/Home/FeaturedTours.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react"
 import Tour from "../Tours/Tour"
 import { useStaticQuery, graphql } from "gatsby"
 import Title from "../Title"
@@ -6,44 +6,44 @@ import styles from "../../css/items.module.css"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 const getTours = graphql`
-    query {
-        featuredTours: allContentfulTour(filter: { featured: { eq: true}}) {
-            edges {
-                node {
-                    name
-                    price
-                    slug
-                    country
-                    contentful_id
-                    days
-                    images {
-                        fluid {
-                            ...GatsbyContentfulFluid_tracedSVG
-                        }
-                    }
-                }
+  query {
+    featuredTours: allContentfulTour(filter: { featured: { eq: true } }) {
+      edges {
+        node {
+          name
+          price
+          slug
+          country
+          contentful_id
+          days
+          images {
+            fluid {
+              ...GatsbyContentfulFluid_tracedSVG
             }
+          }
         }
+      }
     }
+  }
 `
 
 const FeaturedTours = () => {
-    const response = useStaticQuery(getTours)
-    const tours = response.featuredTours.edges
+  const response = useStaticQuery(getTours)
+  const tours = response.featuredTours.edges
 
-    return (
-        <section className={styles.tours}>
-            <Title title="featured" subtitle="tours"/>
-            <div className={styles.center}>
-                {tours.map(({node}) => {
-                    return <Tour key={node.contentful_id} tour={node} />
-                })}
-            </div>
-            <AniLink fade to="/tours" className="btn-primary">
-                all tours
-            </ AniLink>
-        </section>
-    )
+  return (
+    <section className={styles.tours}>
+      <Title title="featured" subtitle="tours" />
+      <div className={styles.center}>
+        {tours.map(({ node }) => {
+          return <Tour key={node.contentful_id} tour={node} />
+        })}
+      </div>
+      <AniLink fade to="/tours" className="btn-primary">
+        all tours
+      </AniLink>
+    </section>
+  )
 }
 
 export default FeaturedTours
