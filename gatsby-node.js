2d7fc34e74ffdec48f5f6a721b1e619ce9ea2b38
Yustian <ray.yustian@gmail.com>
2019-07-14 20:17:19 +0700

    Setup blog pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,26 +1,42 @@
-const path = require("path");
+const path = require("path")
 
-exports.createPages = async({ graphql, actions }) => {
-    const { createPage } = actions
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
 
-    const { data } = await graphql(`
-        query {
-            tours: allContentfulTour {
-                edges {
-                    node {
-                        slug
-                    }
-                }
-            }
+  const { data } = await graphql(`
+    query {
+      tours: allContentfulTour {
+        edges {
+          node {
+            slug
+          }
         }
-    `)
-    data.tours.edges.forEach(({ node }) => {
-        createPage({
-            path: `tours/${node.slug}`,
-            component: path.resolve("./src/templates/tour-template.js"),
-            context: {
-                slug: node.slug,
-            },
-        })
+      }
+      posts: allContentfulPost {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+  data.tours.edges.forEach(({ node }) => {
+    createPage({
+      path: `tours/${node.slug}`,
+      component: path.resolve("./src/templates/tour-template.js"),
+      context: {
+        slug: node.slug,
+      },
     })
+  })
+  data.posts.edges.forEach(({ node }) => {
+    createPage({
+      path: `blog/${node.slug}`,
+      component: path.resolve("./src/templates/blog-template.js"),
+      context: {
+        slug: node.slug,
+      },
+    })
+  })
 }
---
+++ src/templates/blog-template.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Blog = () => {
+  return <div>Blog Template</div>
+}
+
+export default Blog
