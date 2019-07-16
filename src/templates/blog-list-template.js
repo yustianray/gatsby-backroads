b741bf74a26dd8b9390346f4cb86355372a81091
Yustian <ray.yustian@gmail.com>
2019-07-16 08:40:49 +0700

    Blog list template

--- src/templates/blog-list-template.js
+++ src/templates/blog-list-template.js
@@ -6,8 +6,42 @@ import styles from "../css/blog.module.css"
 import BlogCard from "../components/Blog/BlogCard"
 import Title from "../components/Title"
 
-const Blog = () => {
-  return <div>Blogs Template</div>
+const Blog = props => {
+  const { data } = props
+  return (
+    <section className={styles.blog}>
+      <Title title="latest" subtitle="posts" />
+      <div className={styles.center}>
+        {data.posts.edges.map(({ node }) => {
+          return <BlogCard key={node.id} blog={node} />
+        })}
+      </div>
+    </section>
+  )
 }
+
+export const query = graphql`
+  query getPosts($skip: Int!, $limit: Int!) {
+    posts: allContentfulPost(
+      skip: $skip
+      limit: $limit
+      sort: { fields: published, order: DESC }
+    ) {
+      edges {
+        node {
+          slug
+          title
+          id: contentful_id
+          published(formatString: "MMMM Do, YYYY")
+          image {
+            fluid {
+              ...GatsbyContentfulFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Blog
