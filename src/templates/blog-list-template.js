de07f5b6dcecdf6631a6d4f14aaaffba2850baec
Yustian <ray.yustian@gmail.com>
2019-07-25 11:55:46 +0700

    Prev and Next Links

--- src/templates/blog-list-template.js
+++ src/templates/blog-list-template.js
@@ -8,6 +8,11 @@ import Title from "../components/Title"
 
 const Blog = props => {
   const { currentPage, numPages } = props.pageContext
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage =
+    currentPage - 1 === 1 ? `/blogs/` : `/blogs/${currentPage - 1}`
+  const nextPage = `/blogs/${currentPage + 1}`
   const { data } = props
   return (
     <Layout>
@@ -19,6 +24,11 @@ const Blog = props => {
           })}
         </div>
         <section className={styles.links}>
+          {!isFirst && (
+            <AniLink fade to={prevPage} className={styles.link}>
+              Prev
+            </AniLink>
+          )}
           {Array.from({ length: numPages }, (_, i) => {
             return (
               <AniLink
@@ -35,6 +45,11 @@ const Blog = props => {
               </AniLink>
             )
           })}
+          {!isLast && (
+            <AniLink fade to={nextPage} className={styles.link}>
+              Next
+            </AniLink>
+          )}
         </section>
       </section>
     </Layout>
